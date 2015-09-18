3cb84ad8b63746e1e8d128f787c46df4438b6ba6
Dimitrios C. Michalakos <dimitris@jmike.gr>
2015-09-18 15:30:43 +0300

    Rewrite Apktool in es2015

--- src/Apktool.js
+++ src/Apktool.js
@@ -1,58 +1,63 @@
-var os = require('os');
-var path = require('path');
-var exec = require('child_process').exec;
-var Promise = require('bluebird');
-var _ = require('lodash');
-var type = require('type-of');
-var CustomError = require('customerror');
-
+import os from 'os';
+import path from 'path';
+import {exec} from 'child_process';
+import Promise from 'bluebird';
+import _ from 'lodash';
+import type from 'type-of';
+import CustomError from 'customerror';
 
 // resolve path to apktool.jar
-var apktool = path.resolve(__dirname, '../bin/apktool.jar');
+const apktool = path.resolve(__dirname, '../bin/apktool.jar');
 
-/**
- * Decompiles the given .apk file and stores results in the target directory.
- * @param {String} source path to the .apk file
- * @param {String} target path to the directory to store results into
- * @return {Promise} resolving to the path of the decompiled directory
- * @static
- */
-exports.decompile = function (source, target, callback) {
-  var resolver;
-  var cmd;
+class Apktool {
 
-  if (!_.isString(source)) {
-    return Promise.reject(new CustomError('Invalid source argument; expected string, received ' + type(source), 'InvalidArgument'))
-      .nodeify(callback);
+  /**
+   * Decompiles the given .apk file and stores results in the target directory.
+   * @param {String} source path to the .apk file
+   * @param {String} [target=os.tmpdir()] path to the directory to store results into
+   * @param {Function} [callback] optional callback function
+   * @return {Promise} resolving to the path of the decompiled directory
+   * @static
+   */
+  static decompile(source, target, callback) {
+    // validate arguments
+    if (!_.isString(source)) {
+      return Promise.reject(new CustomError(`Invalid source argument; expected string, received ${type(source)}`, 'InvalidArgument'))
+        .nodeify(callback);
+    }
+
+    if (_.isFunction(target)) {
+      callback = target;
+      target = undefined;
+    }
+
+    if (_.isUndefined(target)) {
+      target = path.join(os.tmpdir(), path.basename(source, '.apk'));
+    }
+
+    if (!_.isString(target)) {
+      return Promise.reject(new CustomError(`Invalid target argument; expected string, received ${type(target)}`, 'InvalidArgument'))
+        .nodeify(callback);
+    }
+
+    // resolve relative paths
+    source = path.resolve(source);
+    target = path.resolve(target);
+
+    // compile apktool command
+    const cmd = `java -jar ${apktool} decode --force -c --output=${target} ${source}`;
+
+    // set promise resolver
+    const resolver = (resolve, reject) => {
+      exec(cmd, (err) => {
+        if (err) return reject(err);
+        resolve(target);
+      });
+    };
+
+    return new Promise(resolver).nodeify(callback);
   }
 
-  if (_.isFunction(target)) {
-    callback = target;
-    target = undefined;
-  }
+}
 
-  if (_.isUndefined(target)) {
-    target = path.join(os.tmpdir(), path.basename(source, '.apk'));
-  }
-
-  if (!_.isString(target)) {
-    return Promise.reject(new CustomError('Invalid target argument; expected string, received ' + type(target), 'InvalidArgument'))
-      .nodeify(callback);
-  }
-
-  // resolve relative paths
-  source = path.resolve(source);
-  target = path.resolve(target);
-
-  // compile apktool command
-  cmd = 'java -jar ' + apktool + ' decode --force -c --output=' + target + ' ' + source;
-
-  resolver = function (resolve, reject) {
-    exec(cmd, function (err) {
-      if (err) return reject(err);
-      resolve(target);
-    });
-  };
-
-  return new Promise(resolver).nodeify(callback);
-};
+export default Apktool;
